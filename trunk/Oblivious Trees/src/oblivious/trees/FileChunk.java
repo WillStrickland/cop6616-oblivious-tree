b979c6812e83fdc3511ea40bed8b889a4d56f2a0
c.j.fontaine <c.j.fontaine@0c683ea6-a0f1-f6e4-c841-9f9dfe41a57d>
2011-10-01 20:56:29 +0000

    Refactored Delta to FileChunk

---
+++ trunk/Oblivious Trees/src/oblivious/trees/FileChunk.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oblivious.trees;
+
+/**
+ *
+ * @author ch647274
+ */
+public class FileChunk implements Comparable {
+
+    @Override
+    public int compareTo(Object o) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
