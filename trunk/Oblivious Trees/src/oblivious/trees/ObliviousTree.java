2d8cb38535926481ed95f9d784b4c21e06e8c50c
c.j.fontaine@gmail.com <c.j.fontaine@gmail.com@0c683ea6-a0f1-f6e4-c841-9f9dfe41a57d>
2011-10-04 23:23:27 +0000

    Added ObliviousTree constructor skeleton

--- trunk/Oblivious Trees/src/oblivious/trees/ObliviousTree.java
+++ trunk/Oblivious Trees/src/oblivious/trees/ObliviousTree.java
@@ -10,7 +10,20 @@ package oblivious.trees;
  */
 public class ObliviousTree {
 
+    /*
     public static void main(String[] args) {
         System.out.println("Hello, World!!!");
     }
+    */
+   
+  /**
+   * Constructor generates initial leaf node using
+   * using a given input file.
+   *
+   */
+   
+  public ObliviousTree(FILE input)
+  {
+  }
+  
 }
