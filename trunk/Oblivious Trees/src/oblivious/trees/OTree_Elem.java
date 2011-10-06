88f020ae9d48c8095c24a51e0d04a6d4b76c4b8b
reith.daeva <reith.daeva@0c683ea6-a0f1-f6e4-c841-9f9dfe41a57d>
2011-10-06 00:16:07 +0000

    lots of changes

--- trunk/Oblivious Trees/src/oblivious/trees/OTree_Elem.java
+++ trunk/Oblivious Trees/src/oblivious/trees/OTree_Elem.java
@@ -4,22 +4,13 @@ public abstract class OTree_Elem {
 	final static int MAX_CHILDREN = 3;
 	// Instance properties
 	protected OTree_Elem parent;
-	protected OTree_Elem[] children;
 	
 	// Constructors
 	public OTree_Elem(){
 		parent = null;
-		children = new OTree_Elem[OTree_Elem.MAX_CHILDREN];
-		for (int i=0; i<OTree_Elem.MAX_CHILDREN; i++){
-			children[i] = null;
-		}
 	}
 	public OTree_Elem(OTree_Elem p){
 		parent = p;
-		children = new OTree_Elem[OTree_Elem.MAX_CHILDREN];
-		for (int i=0; i<OTree_Elem.MAX_CHILDREN; i++){
-			children[i] = null;
-		}
 	}
 	
 	// Mutators
@@ -31,42 +22,10 @@ public abstract class OTree_Elem {
 			return false;
 		}
 	}
-	public boolean setChild(int i, OTree_Elem c){
-		if (i>0 || i>OTree_Elem.MAX_CHILDREN || c!=null)
-			return false;
-		this.children[i] = c;
-		return true;
-	}
-	public boolean addChild(OTree_Elem c){
-		boolean success = false;
-		if (c==null)
-			return false;
-		for (int i=0; i<OTree_Elem.MAX_CHILDREN; i++){
-			if (this.children[i] == null){
-				this.children[i] = c;
-				c.parent = this;
-				success = true;
-				break;
-			}
-		}
-		return success;
-	}
-	public boolean removeChild(int i){
-		if (this.children[i] == null){
-			this.children[i] = null;
-			return true;
-		} else {
-			return false;
-		}
-	}
-	protected boolean swapChildren(int i, int j){
-		if (i>0 || j<0 || i>OTree_Elem.MAX_CHILDREN || j>OTree_Elem.MAX_CHILDREN)
-			return false;
-		OTree_Elem tmp = this.children[i];
-		this.children[i] = this.children[j];
-		this.children[j] = tmp;
-		return true;
-	}
+	public abstract boolean setChild(int i, OTree_Elem c);
+	public abstract boolean addChild(OTree_Elem c);
+	public abstract boolean removeChild(int i);
+	protected abstract boolean swapChildren(int i, int j);
 	abstract void calcDegree();
 	protected void trickleDegree(){
 		OTree_Elem tmp = this.parent;
@@ -81,14 +40,6 @@ public abstract class OTree_Elem {
 	public OTree_Elem getParent(){
 		return this.parent;
 	}
-	public OTree_Elem getChild(int i){
-		return this.children[i];
-	}
-	public OTree_Elem[] getChildren(){
-		OTree_Elem[] tmp = new OTree_Elem[OTree_Elem.MAX_CHILDREN];
-		for (int i=0; i<OTree_Elem.MAX_CHILDREN; i++){
-			 tmp[i] = children[i];
-		}
-		return tmp;
-	}
+	public abstract OTree_Elem getChild(int i);
+	public abstract OTree_Elem[] getChildren();
 }
