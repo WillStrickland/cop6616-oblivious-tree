6f0205ac58b89b30f7a035e8852c97fec6b2493d
reith.daeva <reith.daeva@gmail.com>
2011-10-23 23:09:03 +0000

    stuff

--- Oblivious Trees/src/application/TestApplication.java
+++ Oblivious Trees/src/application/TestApplication.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.Signature;
 
@@ -21,6 +22,12 @@ public class TestApplication {
 		// [1] is for verifying
 		Signature[] signatures = initSignature();
 		
+
+	}
+	
+	/** code for opening file and creating OTree from it 
+	 */
+	private static void testOpenFile(){
 		// file to be signed
 		FileInputStream file;
 		String filename = "NONE";
@@ -47,10 +54,25 @@ public class TestApplication {
 		}
 	}
 	
+	
+	
+	/** Initialize psuedorandom number generator
+	 *  @return new psuedorandom number generator, null if error
+	 */
+	private static SecureRandom initPRNG(){	
+		SecureRandom tmp;
+		try {
+			tmp = SecureRandom.getInstance("SHA1PRNG");
+			byte[] b = new byte[1];
+			tmp.nextBytes(b);
+			return tmp;
+		} catch (NoSuchAlgorithmException e){
+			return null;
+		}
+	}
 	/** Generates a public-private key pair at random and
 	 *  returns a signature for signing and another for verifying
-	 * 
-	 *  @return Signature[] index 0 = signing, index 1 = verifying
+	 *  @return Signature[] indices: 0 = signing, 1 = verifying
 	 */
 	private static Signature[] initSignature(){
 		Signature[] sig = new Signature[2];
@@ -71,5 +93,59 @@ public class TestApplication {
 		}
 		return sig;
 	}
+	
+	
+	private static class rndActor implements Runnable{
+		public static final int MAX_SLEEP = 1000;
+		private int actions;
+		
+		public rndActor(){
+			this.actions = 10;
+		}
+		public rndActor(int a){
+			this.actions = a;
+		}
+		public void run(){
+			
+		}
+	}
+	
+	
+	/** code to create a OTree from some random data
+	 *  @return new Oblivious Tree
+	 */
+	private static ObliviousTree createOTree(SecureRandom rnd, Signature signer){
+		int init_size = 10; // initial size in chunks
+		byte[] file = new byte[init_size*ObliviousTree.CHUNK_SIZE];
+		rnd.nextBytes(file);
+		return new ObliviousTree(file, signer);
+	}
+	/** insert a random chunk of data into a random position in an oblivious tree
+	 *  @param o ObliviousTree
+	 *  @param signer signature for signing
+	 */
+	private static void insertOTree(ObliviousTree o, SecureRandom rnd, Signature signer){
+		// position to insert
+		int i;
+		i = rnd.nextInt(o.getSize());
+		// chunk to insert
+		byte[] chunk = new byte[ObliviousTree.CHUNK_SIZE];
+		rnd.nextBytes(chunk);
+		// do insert
+		o.insert(chunk, i, signer);
+	}
+	/** delete a chunk at random from an oblivious tree
+	 *  @param o ObliviousTree
+	 *  @param signer signature for signing
+	 */
+	private static void deleteOTree(ObliviousTree o, SecureRandom rnd, Signature signer){
+		// position to delete
+		int i;
+		i = rnd.nextInt(o.getSize()-1);
+		// do delete
+		o.delete(i, signer);
+	}
+	
+	
 
 }
